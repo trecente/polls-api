f02814bbe92ca924473f8a91a8b2e750abc1a388
Lucas Trecente <lucastrecente@hotmail.com>
2024-02-09 14:02:26 -0300

    feat(http): implement cookie handling and register poll routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,16 +1,20 @@
 import fastify from 'fastify';
+import { createPoll } from './routes/create-poll';
+import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
+import cookie from '@fastify/cookie';
 
 const app = fastify();
 
-app.get('/', () => {
-  return 'Polls'
+app.register(cookie, {
+  secret: 'polls_app',
+  hook: 'onRequest',
 });
 
-app
-  .listen({
-    host: '0.0.0.0',
-    port: 3333,
-  })
-  .then(() => {
-    console.log('🔥 Server running at http://localhost:3333');
-  });
+app.register(getPoll);
+app.register(createPoll);
+app.register(voteOnPoll);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🔥 Server running at http://localhost:3333');
+});
