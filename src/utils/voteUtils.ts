1c8dc2ee5b416d347f93490ef729d3dabc0bbef1
Lucas Trecente <lucastrecente@hotmail.com>
2024-02-09 22:22:10 -0300

    feat(utils): implement utility for retrieving poll votes

---
+++ src/utils/voteUtils.ts
@@ -0,0 +1,16 @@
+import { redis } from '../lib/redis';
+
+export async function getVotes(pollId: string) {
+  const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES');
+
+  const votes = result.reduce((obj, line, index) => {
+    if (index % 2 === 0) {
+      const score = result[index + 1];
+      obj[line] = Number(score);
+    }
+
+    return obj;
+  }, {} as Record<string, number>);
+
+  return votes;
+}
