25173007582782ceffdd9290727a5d990e1402ed
Lucas Trecente <lucastrecente@hotmail.com>
2024-02-10 16:42:13 -0300

    chore(utils): rename voteUtils to polls-votes-fetcher

--- src/utils/poll-votes-fetcher.ts
+++ src/utils/poll-votes-fetcher.ts
@@ -1,6 +1,6 @@
 import { redis } from '../lib/redis';
 
-export async function getVotes(pollId: string) {
+export async function fetchPollVotes(pollId: string) {
   const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES');
 
   const votes = result.reduce((obj, line, index) => {
