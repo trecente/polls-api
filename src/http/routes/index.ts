0ffdce751478d6d85ca3dae6686e445933b83a32
Lucas Trecente <lucastrecente@hotmail.com>
2024-02-10 19:30:59 -0300

    feat(http): integrate websocket endpoint for real-time updates

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { getPoll } from './get-poll';
+import { pollResults } from '../ws/poll-results';
 import { createPoll } from './create-poll';
 import { voteOnPoll } from './vote-on-poll';
 import { deletePoll } from './delete-poll';
 
 export async function Routes(app: FastifyInstance) {
   app.register(getPoll);
+  app.register(pollResults);
   app.register(createPoll);
   app.register(voteOnPoll);
   app.register(deletePoll);
